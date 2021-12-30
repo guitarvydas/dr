5d93c43515a34098580d2b18614882fbed17b120
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-30 11:09:33 -0500

    design rule creates 2 files a-... and b-...

--- dev.bash
+++ dev.bash
@@ -4,8 +4,10 @@ cwd=`pwd`
 plpath=`realpath ../query/`
 cp ~/app/das2f/fb.pl .
 temp=temp_${RANDOM}
+mdfile=dr-edgecontainment.md
+fname=`basename -s '.md' $mdfile`
 #prep "cond\n" "endcond\n" cond.ohm cond.glue --inclusive --stop=1 --support=${cwd}/support.js --split <dr-edgecontainment.md
 prep "cond\n" "endcond\n" cond.ohm cond.glue --inclusive --stop=1 --support=${cwd}/support.js <dr-edgecontainment.md >$temp
-prep "." "$" designrule.ohm designrulea.glue --stop=1 --support=${cwd}/support.js --PLPATH=${plpath} <$temp >dra
-prep "." "$" designrule.ohm designruleb.glue --stop=1 --support=${cwd}/support.js --PLPATH=${plpath} <dr-edgecontainment.md
+prep "." "$" designrule.ohm designrulea.glue --stop=1 --support=${cwd}/support.js --PLPATH=${plpath} <$temp >a-$fname
+prep "." "$" designrule.ohm designruleb.glue --stop=1 --support=${cwd}/support.js --PLPATH=${plpath} <dr-edgecontainment.md >b-$fname
 rm $temp
--- support.js
+++ support.js
@@ -50,43 +50,7 @@ exports.formatJSParameters = function () {
     return result.join ('\n');
 }
 
-
-var conddisplayStack = []; // { predicate, then-lines-as-string }
-
-exports.formatConditionalDisplays = function () {
-    var result = '';
-    var elseif = 'if';
-    conddisplayStack.forEach (cond => {
-	result = `${result}\n${elseif} (${cond.predicate}) { console.log (\`${cond.then}\`);}`;
-	elseif = 'else if';
-    });
-    return result;
-}
-
-exports.pushConditionalDisplay = function (predicateString, thenString) {
-    conddisplayStack.push ({predicate: predicateString.trim (), then: thenString.trim ()});
-    return "";
-}
-
-
-var finaldisplayStack = []; // { predicate, then-lines-as-string }
-
-exports.formatFinalDisplays = function () {
-    var result = '';
-    var elseif = 'if';
-    finaldisplayStack.forEach (cond => {
-	result = `${result}\n${elseif} (${cond.predicate}) { console.log (\`${cond.then}\`);}`;
-	elseif = 'else if';
-    });
-    return result;
-}
-
-exports.pushFinalDisplay = function (predicateString, thenString) {
-    finaldisplayStack.push ({predicate: predicateString.trim (), then: thenString.trim ()});
-    return "";
-}
-
-
+//////////
 
 exports.prefix = function (argv) {
    if (argv.prefix) {
