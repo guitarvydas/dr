17615c61e3442c9a9bfe75c2e9524d1aea6e9a8d
Paul Tarvydas <guitarvydas@gmail.com>
2021-12-30 09:58:56 -0500

    designrulea

--- dev.bash
+++ dev.bash
@@ -2,7 +2,8 @@
 cwd=`pwd`
 plpath=`realpath ../query/`
 cp ~/app/das2f/fb.pl .
+temp=temp${RANDOM}
 #prep "cond\n" "endcond\n" cond.ohm cond.glue --inclusive --stop=1 --support=${cwd}/support.js --split <dr-edgecontainment.md
-prep "cond\n" "endcond\n" cond.ohm cond.glue --inclusive --stop=1 --support=${cwd}/support.js <dr-edgecontainment.md
-prep "." "$" designrule.ohm designrulea.glue --stop=1 --support=${cwd}/support.js --PLPATH=${plpath} <dr-edgecontainment.md
-
+prep "cond\n" "endcond\n" cond.ohm cond.glue --inclusive --stop=1 --support=${cwd}/support.js <dr-edgecontainment.md >$temp
+prep "." "$" designrule.ohm designrulea.glue --stop=1 --support=${cwd}/support.js --PLPATH=${plpath} <$temp
+rm $temp
